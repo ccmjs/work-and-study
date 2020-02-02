9816db7490284c5988ad4e1e07642ef965e335eb
tkless <tmeskh2s@smail.inf.h-brs.de>
2020-02-02 17:36:16 +0100

    correct version

--- learningapp-marketplace/resources/js/app_creator/versions/ccm.app_creator-3.0.0.js
+++ learningapp-marketplace/resources/js/app_creator/versions/ccm.app_creator-3.0.0.js
@@ -490,7 +490,7 @@
         "css": [ "ccm.load", "https://use.fontawesome.com/releases/v5.6.3/css/all.css" ]
       } ],
       "qr_code": [ "ccm.load", "https://ccmjs.github.io/akless-components/libs/qrcode-generator/qrcode.min.js" ],
-      "helper": [ "ccm.load", { "url": "https://ccmjs.github.io/akless-components/modules/helper.mjs", "type": "module" } ],
+      "helper": [ "ccm.load", { "url": "https://ccmjs.github.io/akless-components/modules/versions/helper-1.0.0.mjs" } ],
       "app_url": "https://ccmjs.github.io/work-and-study/learningapp-marketplace/app.html",
       "css": [ "ccm.load", "https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css",
         "https://use.fontawesome.com/releases/v5.6.3/css/all.css",
